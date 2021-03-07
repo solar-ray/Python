f8d29b8a5dfd846fd0ad56fea864371705b87bd6
solar-ray <80153820+solar-ray@users.noreply.github.com>
2021-03-07 15:12:50 +0700

    Add files via upload

---
+++ lesson01/Lesson01task01.py
@@ -0,0 +1,6 @@
+val = 10
+print(val)
+str_val = input("Введите строку:")
+print("Вы ввели", str_val)
+int_val = int(input("Введите число:"))
+print("Вы ввели число", int_val)
---
+++ lesson01/Lesson01task03.py
@@ -0,0 +1,5 @@
+user_num = input("Введите число:")
+user_num2 = user_num + user_num
+user_num3 = user_num + user_num2
+res = int(user_num) + int(user_num2) + int(user_num3)
+print(res)
